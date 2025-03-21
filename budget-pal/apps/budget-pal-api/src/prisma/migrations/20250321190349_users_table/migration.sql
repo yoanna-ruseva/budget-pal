65b3ff41d083d4ec266c66597be4ddfff75a44d7
Yoanna Ruseva <yoanna.ruseva2@gmail.com>
2025-03-21 21:10:28 +0200

    feat: add prisma, initial user schema and doscker-compose.yml file

---
+++ budget-pal/apps/budget-pal-api/src/prisma/migrations/20250321190349_users_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "imgUrl" TEXT,
+    "firts_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
